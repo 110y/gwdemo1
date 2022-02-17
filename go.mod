2af6345e0d11377121c9ca8c3514e63e90d9746d
Yuki Ito <mrno110y@gmail.com>
2022-02-17 21:22:44 +0900

    add main

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/110y/gwdemo1
 
 go 1.18
+
+require github.com/110y/gwdemo3 v0.0.0-20220218011518-502c64a9beed
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/110y/gwdemo3"
+)
+
+func main() {
+	fmt.Println(gwdemo3.Hello("Workspace"))
+}
